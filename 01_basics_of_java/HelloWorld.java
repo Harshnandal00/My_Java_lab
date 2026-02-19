3a6b4924c152c9503c976fece9c406a1adebf00e
Harsh Nandal <harshnandal000@gmail.com>
2026-02-19 16:50:45 +0530

    My first_Java_program

---
+++ 01_basics_of_java/HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld{
+    public static void main(String[] args) {
+        System.out.println("Hello Chatgpt! Thanks for becoming my teacher for my learning of java!");
+    }
+}
