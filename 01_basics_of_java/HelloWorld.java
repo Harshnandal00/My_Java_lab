ca0047407f42640a5fb19298a66b17911e98c5f4
Harsh Nandal <harshnandal000@gmail.com>
2026-02-20 22:37:01 +0530

    Setting up folders for better structure

--- 01_basics_of_java/HelloWorld.java
+++
@@ -1,5 +0,0 @@
-public class HelloWorld{
-    public static void main(String[] args) {
-        System.out.println("Hello Chatgpt! Thanks for becoming my teacher for my learning of java!");
-    }
-}
